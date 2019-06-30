0e60198ab757302f08edc754c272591630ec49a4
wvbe <wybe@x-54.com>
2019-06-30 18:16:11 +0200

    Updates

---
+++ precache-manifest.1f3638e013169991aef3ded884293ec4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40f2b273d479c9caf41b107652ad8e75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3cc60422493e7988781",
+    "url": "/static/js/2.eedac93f.chunk.js"
+  },
+  {
+    "revision": "d65f20768518a280d4d5",
+    "url": "/static/js/main.1f975a67.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
