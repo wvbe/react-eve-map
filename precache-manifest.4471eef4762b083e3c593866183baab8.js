034d6322aa0ff4b92b9d6ac9f37ebe6c95fa33e2
wvbe <wybe@x-54.com>
2019-07-02 00:55:14 +0200

    Updates

---
+++ precache-manifest.4471eef4762b083e3c593866183baab8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bca71b47e5d497592bb9a5e5f27b85c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "869e6cc23c45fc02dc21",
+    "url": "./static/js/2.fe6994fc.chunk.js"
+  },
+  {
+    "revision": "6aec599b8402142cc472",
+    "url": "./static/js/main.41a2d382.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.5fb6cb2c251413b733c66699996ff659.js"
+  "./precache-manifest.4471eef4762b083e3c593866183baab8.js"
 );
 
 self.addEventListener('message', (event) => {
